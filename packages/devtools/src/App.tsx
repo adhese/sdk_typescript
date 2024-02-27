a21cc02e97a13584e4f5e2f4fb3f891eedf92355
Mick Jasker <mick@codecapi.nl>
2024-02-27 13:21:30 +0100

    Make devtools closable

--- packages/devtools/src/App.tsx
+++ packages/devtools/src/App.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { SlotsTable } from './components/slotsTable';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/tabs';
 import { LogTable } from './components/logTable';
+import { Button } from './components/button';
 
 // eslint-disable-next-line ts/naming-convention
 export function App({ adheseContext }: {
@@ -11,43 +12,72 @@ export function App({ adheseContext }: {
 }): ReactElement {
   const [spacing, setSpacing] = useState(0);
   const appRef = useRef<HTMLDivElement | null>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    const resizeObserver = new ResizeObserver(([entry]) => {
-      setSpacing(entry.target.getBoundingClientRect().height);
-    },
-    );
+    let resizeObserver: ResizeObserver | undefined;
 
-    if (appRef.current)
-      resizeObserver.observe(appRef.current);
+    if (isOpen) {
+      resizeObserver = new ResizeObserver(([entry]) => {
+        setSpacing(entry.target.getBoundingClientRect().height);
+      },
+      );
+
+      if (appRef.current)
+        resizeObserver.observe(appRef.current);
+    }
 
     return (): void => {
-      resizeObserver.disconnect();
+      resizeObserver?.disconnect();
     };
-  }, []);
+  }, [isOpen]);
 
   return (
     <div className="adhese-devtools">
-      <div className="fixed bottom-0 w-full border-t-2 border-t-accent p-4 bg-background shadow-lg overflow-auto" ref={appRef}>
-        <Tabs defaultValue="slots">
-          <TabsList>
-            <TabsTrigger value="slots">Slots</TabsTrigger>
-            <TabsTrigger value="logs">Logs</TabsTrigger>
-          </TabsList>
-          <div className="overflow-auto max-h-96 border-t-2 border-t-accent mt-2">
-            <TabsContent value="slots">
-              <SlotsTable adheseContext={adheseContext} />
-            </TabsContent>
-            <TabsContent value="logs">
-              <LogTable adheseContext={adheseContext} />
-            </TabsContent>
-          </div>
-        </Tabs>
-      </div>
-      <div style={{
-        height: spacing,
-      }}
-      />
+
+      {isOpen
+        ? (
+          <>
+            <div
+              className="fixed bottom-0 w-full border-t-2 border-t-accent bg-background shadow-lg overflow-auto"
+              ref={appRef}
+            >
+              <Tabs defaultValue="slots">
+                <div className="flex justify-between  p-4">
+                  <TabsList>
+                    <TabsTrigger value="slots">Slots</TabsTrigger>
+                    <TabsTrigger value="logs">Logs</TabsTrigger>
+                  </TabsList>
+                  <Button
+                    onClick={setIsOpen.bind(null, false)}
+                  >
+                    Close
+                  </Button>
+                </div>
+                <div className="overflow-auto max-h-96 border-t-2 border-t-accent p-4">
+                  <TabsContent value="slots">
+                    <SlotsTable adheseContext={adheseContext} />
+                  </TabsContent>
+                  <TabsContent value="logs">
+                    <LogTable adheseContext={adheseContext} />
+                  </TabsContent>
+                </div>
+              </Tabs>
+            </div>
+            <div style={{
+              height: spacing,
+            }}
+            />
+          </>
+          )
+        : (
+          <Button
+            className="fixed bottom-4 right-4"
+            onClick={setIsOpen.bind(null, true)}
+          >
+            Open Adhese Devtools
+          </Button>
+          )}
     </div>
   );
 }
