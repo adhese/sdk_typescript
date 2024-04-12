7041fc2ab3c6c567fa841cc47e0955c3de5b1903
Mick Jasker <mick@codecapi.nl>
2024-04-12 14:00:59 +0200

    Enable StrictMode for @adhese/sdk-devtools

---
+++ .changeset/light-apricots-join.md
@@ -0,0 +1,5 @@
+---
+"@adhese/sdk-devtools": patch
+---
+
+Enable StrictMode for @adhese/sdk-devtools
--- packages/sdk-devtools/src/main.tsx
+++ packages/sdk-devtools/src/main.tsx
@@ -1,11 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import type { AdheseContext } from '@core';
+import { StrictMode } from 'react';
 import { Devtools } from './Devtools';
 
 export function createAdheseDevtools(element: HTMLElement, context: AdheseContext): () => void {
   const root = createRoot(element);
 
-  root.render(<Devtools adheseContext={context} />);
+  root.render((
+    <StrictMode>
+      <Devtools adheseContext={context} />
+    </StrictMode>
+  ));
 
   return () => {
     root.unmount();
