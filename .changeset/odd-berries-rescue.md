12079c80c88c28d15fe1b5e21d0cd4a3f78e8459
Mick Jasker <mick@codecapi.nl>
2024-06-17 19:04:53 +0200

    Add message to PR with pushed nightly's

---
+++ .changeset/odd-berries-rescue.md
@@ -0,0 +1,5 @@
+---
+"@adhese/sdk": patch
+---
+
+Test patch
--- scripts/nightlyGithubAction.ts
+++ scripts/nightlyGithubAction.ts
@@ -1,8 +1,14 @@
 import { execSync } from 'node:child_process';
+import { env } from 'node:process';
 import { Command } from 'commander';
 import { array, object, string } from 'zod';
+import { Octokit } from 'octokit';
 
 const program = new Command();
+
+const ocktokit = new Octokit({
+  auth: env.GITHUB_TOKEN,
+});
 
 program
   .name('deprecate-nightly')
@@ -10,9 +16,11 @@ program
 
 program
   .option('-P, --packages <package>', 'Package to deprecate')
-  .action((options: {
+  .option('-I, --pull-request <pull-request>', 'Pull request to deprecate')
+  .action(async (options: {
     packages: string;
-  }): void => {
+    pullRequest: number;
+  }): Promise<void> => {
     const packages = array(object({
       name: string(),
       version: string(),
@@ -22,6 +30,18 @@ program
       execSync(`npm deprecate ${pkg.name}@${pkg.version} "This package is a nightly build and is deprecated by default. Please use the latest stable version unless you know what you are doing."`);
       execSync(`git tag -d ${pkg.name}@${pkg.version}`);
       execSync(`git push origin :refs/tags/${pkg.name}@${pkg.version}`);
+    }
+
+    if (packages.length > 0) {
+      await ocktokit.rest.issues.createComment({
+        owner: 'adhese',
+        repo: 'sdk_typescript',
+        // eslint-disable-next-line ts/naming-convention
+        issue_number: Number(options.pullRequest),
+        body: `Created nightly build for the following packages:
+        ${packages.map(pkg => `- \`${pkg.name}@${pkg.version}\``).join('\n')}
+      `,
+      });
     }
   });
 
