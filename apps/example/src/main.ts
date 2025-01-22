ba30e7cee5f9ec7276c216c17eaabaf4a89c4c51
AzureAD\kevinvoortmans <kevin.voortmans@adhese.eu>
2025-01-22 10:33:39 +0100

    create a easy example for uncommonmedia

--- apps/example/src/main.ts
+++ apps/example/src/main.ts
@@ -1,127 +1,37 @@
-import { type AdheseAd, createAdhese } from '@adhese/sdk';
+import { createAdhese } from '@adhese/sdk';
 import { devtoolsPlugin } from '@adhese/sdk-devtools';
-import { stackSlotsPlugin } from '@adhese/sdk-stack-slots';
-
-type CustomAdTag = {
-  type: string;
-  native: {
-    ver: string;
-    assets: ReadonlyArray<{
-      id: number;
-      title?: {
-        text: string;
-      };
-      data?: {
-        type: number;
-        value: string;
-      };
-      img?: {
-        url: string;
-        w: string;
-        h: string;
-      };
-    }>;
-    link: {
-      url: string;
-      fallback: string;
-    };
-  };
-};
-
-function renderHalfwidth(data: CustomAdTag): string {
-  const heading = data.native.assets.find(asset => asset.id === 1)?.title?.text;
-  const image = data.native.assets.find(asset => asset.id === 3)?.img;
-  const backgroundColor = data.native.assets.find(asset => asset.id === 6)?.data?.value;
-  const link = data.native.link.url;
-  const linkText = data.native.assets.find(asset => asset.id === 2)?.data?.value;
-
-  return `
-    <a href="${link}" style="
-      background-image: url(${image?.url});
-      height: ${image?.h}px;
-      background-size: contain;
-      background-position: right center;
-      background-repeat: no-repeat;
-      background-color: ${backgroundColor};
-      padding: 20px;
-      max-width: ${image?.w}px;
-      display: block">
-      <div style="
-        max-width: 40%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        height: 100%;">
-        <h2 style="font-weight: bold; font-size: 24px;">${heading}</h2>
-        <p>${linkText}</p>
-      </div>
-    </a>`;
-}
 
 const adhese = createAdhese({
-  account: 'demo',
+  account: 'uncommonmedia',
+  host: "https://ads-uncommonmedia.staging-adhese.com",
+  poolHost: "https://pool-uncommonmedia.staging-adhese.com",
   debug: true,
   consent: true,
-  initialSlots: [
-    {
-      format: 'skyscraper',
-      containingElement: 'skyscraper',
-    },
-    {
-      format: 'halfwidthsmallresponsive',
-      containingElement: 'halfwidthsmallresponsive',
-      renderMode: 'inline',
-      setup(_, { onBeforeRender }): void {
-        onBeforeRender((data) => {
-          const ad = data as AdheseAd<CustomAdTag>;
-
-          if (typeof ad.tag === 'string')
-            return ad as AdheseAd<string>;
-
-          return {
-            ...ad,
-            tag: renderHalfwidth(ad.tag),
-          };
-        });
-      },
-    },
-  ],
-  location: '_sdk_example_',
-  plugins: [devtoolsPlugin, stackSlotsPlugin],
+  location: '_home_',
+  plugins: [devtoolsPlugin],
+  parameters: {
+    "br": ["test"]
+  }
 });
 
 window.adhese = adhese;
 
-adhese.plugins.stackSlots.addSlot({
-  format: 'newstack',
-  containingElement: 'newstack',
-  maxAds: 3,
-  setup(_, hooks) {
-    hooks.onBeforeRender((data) => {
-      const ad = data as AdheseAd<ReadonlyArray<CustomAdTag>>;
-
-      if (typeof ad.tag === 'string')
-        return ad as AdheseAd<string>;
-
-      return {
-        ...ad,
-        tag: ad.tag.map(renderHalfwidth).join(''),
-      };
-    });
-  },
-});
-
 adhese.addSlot({
-  format: 'leaderboard',
-  containingElement: 'leaderboard',
+  format: '300x600',
+  slot: "_one_",
+  containingElement: 'halfpage1',
   renderMode: 'inline',
+  parameters: {
+    "ps": "top"
+  }
 });
 
 adhese.addSlot({
-  format: 'imu',
-  containingElement: 'imu',
-  lazyLoading: true,
-  lazyLoadingOptions: {
-    rootMargin: '0px',
-  },
+  format: '300x600',
+  slot: "_one_",
+  containingElement: 'halfpage2',
+  renderMode: 'inline',
+  parameters: {
+    "ps": "bottom"
+  }
 });
