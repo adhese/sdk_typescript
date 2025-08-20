1cf343030c3cb1a028c6c5211f48c04c71ea8bd5
AzureAD\kevinvoortmans <kevin.voortmans@adhese.eu>
2025-08-20 17:23:54 +0200

    cleaned main.ts

--- apps/example/src/main.ts
+++ apps/example/src/main.ts
@@ -1,62 +1,6 @@
-import { type AdheseAd, createAdhese } from '@adhese/sdk';
+import { createAdhese } from '@adhese/sdk';
 import { devtoolsPlugin } from '@adhese/sdk-devtools';
 import { stackSlotsPlugin } from '@adhese/sdk-stack-slots';
-
-type CustomAdTag = {
-  type: string;
-  native: {
-    ver: string;
-    assets: ReadonlyArray<{
-      id: number;
-      title?: {
-        text: string;
-      };
-      data?: {
-        type: number;
-        value: string;
-      };
-      img?: {
-        url: string;
-        w: string;
-        h: string;
-      };
-    }>;
-    link: {
-      url: string;
-      fallback: string;
-    };
-  };
-};
-
-function renderHalfwidth(data: CustomAdTag): string {
-  const heading = data.native.assets.find(asset => asset.id === 1)?.title?.text;
-  const image = data.native.assets.find(asset => asset.id === 3)?.img;
-  const backgroundColor = data.native.assets.find(asset => asset.id === 6)?.data?.value;
-  const link = data.native.link.url;
-  const linkText = data.native.assets.find(asset => asset.id === 2)?.data?.value;
-
-  return `
-    <a href="${link}" style="
-      background-image: url(${image?.url});
-      height: ${image?.h}px;
-      background-size: contain;
-      background-position: right center;
-      background-repeat: no-repeat;
-      background-color: ${backgroundColor};
-      padding: 20px;
-      max-width: ${image?.w}px;
-      display: block">
-      <div style="
-        max-width: 40%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        height: 100%;">
-        <h2 style="font-weight: bold; font-size: 24px;">${heading}</h2>
-        <p>${linkText}</p>
-      </div>
-    </a>`;
-}
 
 const adhese = createAdhese({
   account: 'demo',
