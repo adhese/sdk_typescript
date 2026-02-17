ba91cc913b2be2badc320c30bb2daf82f141654a
Tim Sturtewagen <tim@adhese.eu>
2026-02-17 12:50:39 +0100

    Support for all origin values coming from gateway markets
    Instead of only accepting responses from DALE or JERLICIA, we need support for all possible origin values.

--- packages/server-mocking/src/mocks/adRequest.ts
+++ packages/server-mocking/src/mocks/adRequest.ts
@@ -71,4 +71,35 @@ export const adRequestHandlers = [
       id: slot.slotname,
     }))));
   }),
+  http.post('https://ads-gateway.adhese.com/json', async ({
+    request,
+  }) => {
+    const requestBody = await request.json() as {
+      slots: ReadonlyArray<{
+        slotname: string;
+        parameters?: Record<string, ReadonlyArray<string> | string>;
+      }>;
+      parameters?: Record<string, ReadonlyArray<string> | string>;
+    };
+
+    return new HttpResponse(JSON.stringify(requestBody.slots.map(slot => ({
+      origin: 'ADFORM',
+      originInstance: '',
+      ext: 'js',
+      // eslint-disable-next-line ts/naming-convention
+      slotID: slot.slotname,
+      slotName: slot.slotname,
+      adType: 'image',
+      originData: {
+        seatbid: [{ bid: [{ dealid: 'DID-3295-233118', crid: '87294141', ext: { prebid: { type: 'banner' } } }], seat: '9412' }],
+      },
+      width: '656',
+      height: '164',
+      body: '<script src="https://track.adform.net/adfscript/?bn=87294141"></script><div style="position:absolute;left:0;top:0;visibility:hidden"><img src="https://ads-demo.adhese.com/rtb_gateway/handlers/client/track?id=test" width="1" height="1" alt="" style="display:none"></div>',
+      extension: {
+        mediaType: 'banner',
+        prebid: { cpm: { amount: '0.3198538325072811054', currency: 'USD' } },
+      },
+    }))));
+  }),
 ];
